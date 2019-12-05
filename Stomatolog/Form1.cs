932e608dc47d16e44c815bfeb06a1f42fcef3617
florauson <56885510+florauson@users.noreply.github.com>
2019-12-06 00:04:56 +0100

    zajecia 04.12

--- Stomatolog/Form1.cs
+++ Stomatolog/Form1.cs
@@ -162,115 +162,37 @@ namespace Stomatolog
             public int w;
 
         }
-        private void obliczToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var DR = DTklienci.Select();
-            int Lp = 0;
-            List<CDesc> Lst = new List<CDesc>();
-
-
-
-            foreach (var row in DR)
-            {
-                string sLP = row["Lp"].ToString();
-                string sNruslugi = row["numeruslugi"].ToString();
-                var DR2 = DTuslugi.Select("Lp = '" + sNruslugi + "'");
-
-                for (int i = 0; i <= DR2.Length - 2; i++)
-                {
-                    var rec1 = DR2[i];
-                    CDesc el1 = new CDesc();
-                    el1.czasusl = double.Parse(rec1["czas trwania"].ToString());
-                    el1.przerwa = double.Parse(rec1["minOdstep"].ToString()) * 8 * 60;
-                    el1.nast = Lp + 1;
-                    Lst.Add(el1);
-                    Lp++;
-                }
-
-                var rec = DR2[DR2.Length - 1];
-                CDesc el = new CDesc();
-                el.czasusl = double.Parse(rec["czas trwania"].ToString());
-                el.przerwa = double.Parse(rec["minOdstep"].ToString()) * 8 * 60;
-                el.nast = 0;
-                Lst.Add(el);
-                Lp++;
-
-            }
-            int[] pi = new int[Lp + 2];
-            for (int i = 0; i <= Lp; i++)
-            {
-                pi[i] = i;
-
-            }
-            pi[0] = pi[Lp + 1] = 0;
-
-
-
-            List<cp>[] Nast = new List<cp>[Lp + 2];
-            List<cp>[] Pop = new List<cp>[Lp + 2];
-            for (int i = 0; i < Lp + 1; i++)
-            {
-                Nast[i] = new List<cp>();
-                Pop[i] = new List<cp>();
-            }
-            for (int s = 0; s < Lst.Count - 1; s++)
-            {
-                int nst = Lst[s].nast + 1;
-                int w = (int)(Lst[s].przerwa);
-                if (Lst[s].nast == 0)
-                    continue;
-                cp ee = new cp(); ee.nst = nst; ee.w = w;
-                Nast[s + 1].Add(ee);
-                cp p = new cp(); p.nst = s + 1; p.w = w;
-                Pop[nst].Add(p);
-
-
-
-            }
-            for (int s = 0; s < Lp + 1; s++)
-            {
-                int node = pi[s];
-                int nst = pi[s + 1];
-                if (node == 0) continue;
-                if (nst == 0) continue;
-                int w = 0;
-
-                cp ee = new cp(); ee.nst = nst; ee.w = w;
-                Nast[node].Add(ee);
-                cp p = new cp(); p.nst = node; p.w = w;
-                Pop[nst].Add(p);
-
-
-
-            }
-            int[] LP = new int[Lp + 1];
+        public int harm(int Lp, List<CDesc> Lst, List<cp>[] Pop, List<cp>[] Nast, int[] C, int [] S)
+{
+        int[] LP = new int[Lp + 1];
             for (int i = 1; i <= Lp; i++)
             {
                 LP[i] = Pop[i].Count;
             }
 
-            Queue<int> Q = new Queue<int>();
+    Queue<int> Q = new Queue<int>();
 
             for (int i = 1; i <= Lp; i++)
             {
                 if (LP[i] == 0)
                     Q.Enqueue(i);
             }
-            int[] C = new int[Lp + 1];
             for (int i = 0; i <= Lp; i++)
             {
                 C[i] = 0;
             }
-            int[] S = new int[Lp + 1];
+            
             for (int i = 0; i <= Lp; i++)
             {
                 S[i] = 0;
             }
-            while (Q.Count > 0)
+            int iter = 0;
+            while (Q.Count>0)
             {
+                iter++;
                 int node = Q.Dequeue();
-                C[node] = S[node] + (int)(Lst[node - 1].czasusl);
-                foreach (var ns in Nast[node])
+C[node] = S[node] + (int) (Lst[node - 1].czasusl);
+                foreach(var ns in Nast[node])
                 {
                     if (--LP[ns.nst] == 0)
                         Q.Enqueue(ns.nst);
@@ -278,15 +200,141 @@ namespace Stomatolog
                 foreach (var ns in Nast[node])
                 {
                     if (ns.nst != 0)
-                        S[ns.nst] = Math.Max(S[ns.nst], C[node] + ns.w);
+                        S[ns.nst]=Math.Max(S[ns.nst], C[node]+ns.w);
                 }
             }
+            if (iter < Lp) return 0;
+            return 1;
+    }
 
+        public void createGraph(int Lp, List<CDesc> Lst, int [] pi, out List<cp>[]  Nast, out List<cp>[] Pop)
+        {
+           Nast = new List<cp>[Lp + 2];
+           Pop = new List<cp>[Lp + 2];
+            for (int i = 0; i<Lp + 1; i++)
+            {
+                Nast[i] = new List<cp>();
+                Pop[i] = new List<cp>();
+            }
+            for(int s=0; s<Lst.Count - 1; s++)
+            {
+                int nst = Lst[s].nast + 1;
+    int w = (int)(Lst[s].przerwa);
+                if (Lst[s].nast == 0)
+                    continue;
+                cp ee = new cp(); ee.nst = nst; ee.w = w;
+                Nast[s + 1].Add(ee);
+    cp p = new cp(); p.nst = s+1; p.w = w;
+                Pop[nst].Add(p);
 
+}
+            for (int s = 0; s<Lp+1; s++)
+            {
+                int node = pi[s];
+int nst = pi[s + 1];
+                if (node == 0) continue;
+                if (nst == 0) continue;
+                int w = 0;
 
+cp ee = new cp(); ee.nst = nst; ee.w = w;
+                Nast[node].Add(ee);
+cp p = new cp(); p.nst =node; p.w = w;
+                Pop[nst].Add(p);
 
+            }
         }
 
+        public int cmax(int Lp, int [] C)
+        {
+
+            int Cmax = 0;
+            for (int i=0; i<=Lp; i++)
+            {
+
+                if (C[i] > Cmax)
+                    Cmax = C[i];
+            }
+            return Cmax;
+        }
+        private void obliczToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var DR = DTklienci.Select();
+            int Lp = 0;
+            List<CDesc> Lst = new List<CDesc>();
+
+    
+        
+            foreach(var row in DR)
+            {
+                string sLP = row["Lp"].ToString();
+                string sNruslugi = row["numeruslugi"].ToString();
+                var DR2 = DTuslugi.Select("Lp = '"+sNruslugi+"'");
+                
+                for (int i=0;i<=DR2.Length-2;i++)
+                {
+                    var rec1 = DR2[i];
+                    CDesc el1 = new CDesc();
+                    el1.czasusl = double.Parse(rec1["czas trwania"].ToString());
+                    el1.przerwa = double.Parse(rec1["minOdstep"].ToString()) * 8 * 60;
+                    el1.nast = Lp+1;
+                    Lst.Add(el1);
+                    Lp++;
+                }
+
+                var rec = DR2[DR2.Length - 1];
+                CDesc el = new CDesc();
+                el.czasusl = double.Parse(rec["czas trwania"].ToString());
+                el.przerwa = double.Parse(rec["minOdstep"].ToString())*8*60;
+                el.nast = 0;
+                Lst.Add(el);
+                Lp++;
+
+            }
+            int []pi = new int[Lp + 2];
+            for(int i=0;i<=Lp;i++)
+            {
+                pi[i] = i;
+
+            }
+            pi[0] = pi[Lp + 1] = 0;
+            //pi[3] = 2;
+            //pi[2] = 3;
+
+            List<cp>[] Nast = new List<cp>[Lp + 2];
+            List<cp>[] Pop = new List<cp>[Lp + 2];
+            et:;
+            createGraph(Lp, Lst, pi, out Nast, out Pop);
+            int[] C = new int[Lp + 1];
+            int[] S = new int[Lp + 1];
+            int res = harm(Lp, Lst, Pop, Nast, C, S);
+            int Cmax0=cmax(Lp, C);
+            int bps = 0;
+            int bCmax = 1000000;
+            for( int ps =1; ps<Lp-1;ps++)
+            {
+                int sw = pi[ps]; pi[ps] = pi[ps + 1];  pi[ps + 1] = sw;
+                createGraph(Lp, Lst, pi, out Nast, out Pop);
+               
+                if(harm(Lp, Lst, Pop, Nast, C, S)==1)
+                {
+                    int Cmax = cmax(Lp, C);
+                    if(Cmax<bCmax)
+                    {
+                        bCmax = Cmax;
+                        bps = ps;
+                    }
+                }
+
+                sw = pi[ps]; pi[ps] = pi[ps + 1]; pi[ps + 1] = sw;
+            }
+            if(bCmax < Cmax0)
+            {
+               int sw = pi[bps]; pi[bps] = pi[bps + 1]; pi[bps + 1] = sw;
+                goto et;
+            }
+
+    
+        }
 
         private void Klienci_Click(object sender, EventArgs e)
         {
